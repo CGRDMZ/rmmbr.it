d7fee221ac5202fce71d764e90872f70a330dcfc
cgrdmz <cgrdmzz@gmail.com>
2022-06-26 18:27:18 +0300

    fix inner error leaking in prod

--- sherrors/utils.go
+++ sherrors/utils.go
@@ -6,15 +6,22 @@ import (
 
 func HandleError(err error) *CustomError {
 	switch e := err.(type) {
+
 	case CustomError:
+		if config.Conf.Env == "PROD" {
+			e.InnerError = nil
+		}
 		return &e
 	case *CustomError:
+		if config.Conf.Env == "PROD" {
+			e.InnerError = nil
+		}
 		return e
 	default:
 		if config.Conf.Env == "PROD" {
 			return ErrInternalServer
 		}
-		
+
 		intErr := InternalError(e)
 		return &intErr
 	}
